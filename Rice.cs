980837e27fcd0994de780e1109ba5eece2d89388
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-29 14:19:15 +0530

    Rice File

---
+++ Rice.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryDataManagement
+{
+    class Rice
+    {
+        public string name;
+        public string weight;
+        public int Price;
+
+    }
+}
