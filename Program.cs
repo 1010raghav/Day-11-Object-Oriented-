2fd492449f380c4650e84e1bafc6224878f15536
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-29 14:15:06 +0530

    Main Program file

--- Program.cs
+++ Program.cs
@@ -9,9 +9,19 @@ namespace InventoryDataManagement
         {
             FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
 
-            var data = fetchJsonForRice.Read("E:/Json/Rice.json");
-
+            var data = fetchJsonForRice.Read("E:/bidgelabz/Day11 Object Oriented Practice/Rice.JSON");
             Console.WriteLine(JsonConvert.SerializeObject(data));
+
+            FetchJsonForWheat fetchJsonForWheat = new FetchJsonForWheat();
+
+            var data1 = fetchJsonForWheat.Read("E:/bidgelabzDay11 Object Oriented Practice/Wheat.JSON");
+            Console.WriteLine(JsonConvert.SerializeObject(data1));
+
+            FetchJsonForPulses fetchJsonForPulses = new FetchJsonForPulses();
+
+            var data2 = fetchJsonForPulses.Read("E:/bidgelabz/Day11 Object Oriented Practice/Pulses.JSON");
+            Console.WriteLine(JsonConvert.SerializeObject(data2));
+        
         }
     }
 }
